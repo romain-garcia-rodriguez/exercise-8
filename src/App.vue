147d63a944e94509fb57aa996ddcfe370f80119f
romain-garcia-rodriguez <romain.garcia@hotmail.com>
2023-03-27 07:53:18 +0200

    Init exercise 8

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <!-- Pour cet exercice, il faut créer un composant modal en utilsant les slots
+  Les resources pour les modal sont dans le dossier assets:
+    1. modal -> 2. modal-active 3. modal-submitted
+  Pour pouvoir ouvrir la modal, ajouter un bouton dans la page, au clique, ouvrir la modal -->
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
